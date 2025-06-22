2e7da77e4e4e826fee2938ba779def97cd50428f
Hikaru <portuguese_d029@163.com>
2025-06-23 00:33:21 +0800

    ft(relation): add model Relation

--- relation/models.py
+++ relation/models.py
@@ -13,3 +13,16 @@ class Member(models.Model):
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     gender = models.CharField(max_length=8, choices=GENDERS)
     birth_date = models.DateField(blank=True, null=True)
+
+
+class Relation(models.Model):
+    RELATION_TYPES = (
+        ('P', _('Parent')),
+        ('S', _('Spouse')),
+    )
+    member1 = models.ForeignKey(
+        Member, related_name='member1', on_delete=models.CASCADE)
+    member2 = models.ForeignKey(
+        Member, related_name='member2', on_delete=models.CASCADE)
+    relation_type = models.CharField(max_length=32, choices=RELATION_TYPES)
+    start_date = models.DateField(null=True, blank=True)

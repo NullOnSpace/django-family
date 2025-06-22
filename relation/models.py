7c4392f3bf70183f91d49daa976b0e27cb2010f0
Hikaru <portuguese_d029@163.com>
2025-06-23 00:07:50 +0800

    ft(relation): add model Member

--- relation/models.py
+++ relation/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Member(models.Model):
+    GENDERS = (
+        ('M', _('Male')),
+        ('F', _('Female')),
+        ('U', _('Unknown')),
+    )
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    gender = models.CharField(max_length=8, choices=GENDERS)
+    birth_date = models.DateField(blank=True, null=True)

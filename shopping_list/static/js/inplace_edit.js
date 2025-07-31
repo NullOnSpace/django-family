93317649e23de0c5dd814c0d4f943df16b52f8fa
Hikaru <portuguese_d029@163.com>
2025-07-31 16:48:37 +0800

    temp commit

--- shopping_list/static/js/inplace_edit.js
+++ shopping_list/static/js/inplace_edit.js
@@ -1,28 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     console.log('页面加载完成');
-    // 处理切换器的逻辑
-    let switchers = document.querySelectorAll('.switcher');
-    switchers.forEach(function (switcher) {
-        let buttons = switcher.querySelector(".switcher-buttons");
-        let contents = switcher.querySelector(".switcher-contents");
-        let activeButton = buttons.querySelector(".active-button");
-        buttons.addEventListener('click', function (event) {
-            if (event.target.classList.contains('switcher-button')) {
-                // 切换按钮状态
-                activeButton.classList.remove('active-button');
-                event.target.classList.add('active-button');
-                activeButton = event.target;
-
-                // 切换内容显示
-                contents.querySelectorAll('.switcher-content').forEach(function (content) {
-                    content.classList.remove('active-content');
-                });
-                let targetContentId = event.target.getAttribute('data-target');
-                let targetContent = contents.querySelector('#' + targetContentId);
-                targetContent.classList.add('active-content');
-            }
-        });
-    });
+    
     // 展示清单的表项和表单间的转化
     let displayInputToggles = document.querySelectorAll('.display-input-toggle');
     displayInputToggles.forEach(function (toggle) {

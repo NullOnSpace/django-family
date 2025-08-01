a98ab75357d53068505aa8d61ea432ffa9b2b016
Hikaru <portuguese_d029@163.com>
2025-08-01 15:37:46 +0800

    ft(babycare): add more methods for model babydate and add test for them

--- babycare/models.py
+++ babycare/models.py
@@ -4,34 +4,148 @@ from django.db import models
 from django.utils import timezone
 
 
+class EarlierThanLMPError(Exception):
+    """Exception raised when the date is earlier than the last menstrual period."""
+    pass
+
+
+class LaterThanBirthError(Exception):
+    """Exception raised when the date is later than the birthday."""
+    pass
+
+
+class NotBornError(Exception):
+    """Exception raised when trying to calculate age for a baby that has not been born yet."""
+    pass
+
+
 class BabyDate(models.Model):
     last_menstrual_period = models.DateField()  # 末次月经
-    estimated_due_date = models.DateField()  # 推测预产期
+    estimated_due_date = models.DateField(null=True, blank=True)  # 推测预产期
     birthday = models.DateTimeField(null=True, blank=True)  # 出生时间
-    ultrasound_fixed_days = models.DurationField(
-        default=datetime.timedelta())  # 超声定位反推的孕周初始日期 正数代表比末次月经晚，负数代表比末次月经早
+    ultrasound_fixed_days = models.IntegerField(
+        default=0)  # 超声定位矫正的天数 正数代表比末次月经晚，负数代表比末次月经早
+
+    def days_to_lmp(self, date: datetime.date | None = None) -> int:
+        """
+        计算距离末次月经的天数
+        :param date: 计算距离末次月经的日期，默认为当前日期
+        :return: 距离末次月经的天数
+        """
+        if date is None:
+            date = timezone.now().date()
+        if self.last_menstrual_period > date:
+            raise EarlierThanLMPError(
+                f"The date: {date} is earlier than the last menstrual period{self.last_menstrual_period}.")
+        return (date - self.last_menstrual_period).days
 
     def get_gestational_age_days(self, date:  datetime.date | None = None, ultrasound_fixed: bool = False) -> int:
         """
         计算孕周的天数
-        (fixme) 晚于出生的日期或早于末次月经的日期会导致计算错误
+        晚于出生的日期抛出异常 LaterThanBirthError
+        早于末次月经的日期抛出异常 EarlierThanLMPError
         :param date: 计算孕周的日期，默认为当前日期
+        :param ultrasound_fixed: 是否使用超声定位的孕周初始日期
         :return: 孕周天数
         """
         if date is None:
             date = timezone.now().date()
-        date_diff = date - self.last_menstrual_period
+        if self.birthday and date > self.birthday.date():
+            raise LaterThanBirthError(
+                f"The date for GA: {date} is later than the birthday: {self.birthday}.")
+        days = self.days_to_lmp(date)
         if ultrasound_fixed:
-            date_diff -= self.ultrasound_fixed_days
-        return date_diff.days
+            days -= self.ultrasound_fixed_days
+        return days
+
+    def get_postmenstrual_age_days(self, date: datetime.date | None = None) -> int:
+        """
+        计算月经后年龄天数(PMA)，即胎龄加上出生后实际年龄
+        早于出生的日期抛出异常 NotBornError
+        :param date: 计算月经后年龄的日期，默认为当前日期
+        :return: 月经后年龄天数
+        """
+        if date is None:
+            date = timezone.now().date()
+        if self.birthday is None or date < self.birthday.date():
+            raise NotBornError(
+                f"The date for PMA: {date} is earlier than the birthday: {self.birthday}.")
+        return (date - self.last_menstrual_period).days
+
+    def get_chronological_age_days(self, date: datetime.date | None = None) -> int:
+        """
+        计算实际年龄天数，即距离出生日期的天数
+        早于出生日期的日期抛出异常 NotBornError
+        :param date: 计算实际年龄的日期，默认为当前日期
+        :return: 实际年龄天数
+        """
+        if date is None:
+            date = timezone.now().date()
+        if not self.is_born(date):
+            raise NotBornError(
+                f"The date for chronological age: {date} is earlier than the birthday: {self.birthday}.")
+        else:
+            return (date - self.birthday.date()).days  # type: ignore
+
+    def get_corrected_age_days(self, date: datetime.date | None = None) -> int:
+        """
+        计算矫正年龄天数
+        如果是早产，则使用预产期计算矫正年龄的天数
+        如果是足月出生，则返回实际年龄
+        如果尚未出生，则抛出异常 NotBornError
+        :param date: 计算矫正年龄的日期，默认为当前日期
+        :return: 矫正年龄天数
+        """
+        if date is None:
+            date = timezone.now().date()
+        if self.is_born(date):
+            if self.is_preterm():
+                return -self.days_to_due(date)
+            else:
+                return self.get_chronological_age_days(date)
+        else:
+            raise NotBornError(
+                f"The date for corrected age: {date} is earlier than the birthday: {self.birthday}.")
 
     def days_to_due(self, date: datetime.date | None = None) -> int:
         """
         计算距离预产期的天数
+        出生前用于计算距离预产期的天数(为正数)
+        出生后用于计算早产儿的矫正年龄(为负数)
+        早于末次月经的日期抛出异常 EarlierThanLMPError
         :param date: 计算距离预产期的日期，默认为当前日期
         :return: 距离预产期的天数
         """
         if date is None:
             date = timezone.now().date()
-        return (self.estimated_due_date - date).days
-        
+        if self.last_menstrual_period > date:
+            raise EarlierThanLMPError(
+                f"The date: {date} is earlier than the last menstrual period: {self.last_menstrual_period}.")
+        return (self.get_due_date() - date).days
+
+    def get_due_date(self) -> datetime.date:
+        """
+        获取预产期
+        如果没有设置预产期，则根据末次月经推算
+        :return: 预产期
+        """
+        if self.estimated_due_date is None:
+            # 默认推算280天
+            return self.last_menstrual_period + datetime.timedelta(days=280)
+        return self.estimated_due_date
+
+    def is_born(self, date: datetime.date | None = None) -> bool:
+        """
+        判断是否已经出生
+        :return: 如果已经出生，返回True，否则返回False
+        """
+        if date is None:
+            date = timezone.now().date()
+        return self.birthday is not None and self.birthday.date() < date
+
+    def is_preterm(self) -> bool:
+        """
+        判断是否为早产
+        :return: 如果是早产，返回True，否则返回False
+        """
+        return self.is_born() and self.get_gestational_age_days(self.birthday.date()) < 259  # type: ignore
--- babycare/tests.py
+++ babycare/tests.py
@@ -1,3 +1,122 @@
 from django.test import TestCase
+from django.utils import timezone
 
-# Create your tests here.
+from babycare.models import BabyDate, EarlierThanLMPError, LaterThanBirthError, NotBornError
+
+
+class BabyDateTestCase(TestCase):
+    def test_get_gestational_age_days(self):
+        baby_date = BabyDate(
+            last_menstrual_period=timezone.now().date() - timezone.timedelta(days=30),
+        )
+        # Test with current date
+        self.assertEqual(baby_date.get_gestational_age_days(), 30)
+
+        # Test with ultrasound fixed days
+        baby_date.ultrasound_fixed_days = 5
+        self.assertEqual(baby_date.get_gestational_age_days(
+            ultrasound_fixed=True), 25)
+
+        # Test with a date before the last menstrual period
+        date_earlier = timezone.now().date() - timezone.timedelta(days=32)
+        with self.assertRaises(EarlierThanLMPError):
+            baby_date.get_gestational_age_days(date_earlier)
+
+    def test_get_gestational_age_days_with_birthday(self):
+        birthday = timezone.now() - timezone.timedelta(days=10)
+        baby_date = BabyDate(
+            last_menstrual_period=timezone.now().date() - timezone.timedelta(days=270),
+            birthday=birthday
+        )
+        # Test with a date after the birthday
+        with self.assertRaises(LaterThanBirthError):
+            baby_date.get_gestational_age_days()
+
+        # Test with a date before the birthday
+        date_before_birthday = (birthday - timezone.timedelta(days=5)).date()
+        self.assertEqual(baby_date.get_gestational_age_days(
+            date_before_birthday), 255)
+
+    def test_get_postmenstrual_age_days(self):
+        baby_date = BabyDate(
+            last_menstrual_period=timezone.now().date() - timezone.timedelta(days=281),
+        )
+        # Test with current date without birthday
+        with self.assertRaises(NotBornError):
+            baby_date.get_postmenstrual_age_days()
+
+        baby_date.birthday = timezone.now() - timezone.timedelta(days=10)
+
+        # Test with a date before birthday
+        date_earlier = timezone.now().date() - timezone.timedelta(days=32)
+        with self.assertRaises(NotBornError):
+            baby_date.get_postmenstrual_age_days(date_earlier)
+
+        # Test with current date after birthday
+        self.assertEqual(baby_date.get_postmenstrual_age_days(), 281)
+
+    def test_get_chronological_age_days(self):
+        baby_date = BabyDate(
+            last_menstrual_period=timezone.now().date() - timezone.timedelta(days=300)
+        )
+        # Test with current date without birthday
+        with self.assertRaises(NotBornError):
+            baby_date.get_chronological_age_days()
+
+        baby_date.birthday = timezone.now() - timezone.timedelta(days=10)
+        # Test with a date before the birthday
+        date_before_birthday = (baby_date.birthday -   # type: ignore
+                                timezone.timedelta(days=5)).date()
+        with self.assertRaises(NotBornError):
+            baby_date.get_chronological_age_days(date_before_birthday)
+
+        # Test with a date after the birthday
+        date_after_birthday = (baby_date.birthday +  # type: ignore
+                               timezone.timedelta(days=5)).date()
+        self.assertEqual(
+            baby_date.get_chronological_age_days(date_after_birthday),
+            5
+        )
+
+    def test_get_corrected_age_days_with_term_baby(self):
+        baby_date = BabyDate(
+            last_menstrual_period=timezone.now().date() - timezone.timedelta(days=300)
+        )
+        # Test with current date without birthday
+        with self.assertRaises(NotBornError):
+            baby_date.get_corrected_age_days()
+
+        # Test with term baby
+        baby_date.birthday = timezone.now() - timezone.timedelta(days=10)
+        # Test with a date before the birthday
+        date_before_birthday = (baby_date.birthday - # type: ignore
+                                timezone.timedelta(days=5)).date()
+        with self.assertRaises(NotBornError):
+            baby_date.get_corrected_age_days(date_before_birthday)
+        
+        # Test with a date after the birthday
+        date_after_birthday = (baby_date.birthday +  # type: ignore
+                               timezone.timedelta(days=5)).date()
+        self.assertEqual(
+            baby_date.get_corrected_age_days(date_after_birthday),
+            5
+        )
+
+    def test_get_corrected_age_days_with_preterm_baby(self):
+        birthday = timezone.now() - timezone.timedelta(days=50)
+        baby_date = BabyDate(
+            last_menstrual_period=timezone.now().date() - timezone.timedelta(days=300),
+            birthday=birthday
+        )
+
+        date_after_birthday = (birthday + timezone.timedelta(days=5)).date()
+        self.assertEqual(
+            baby_date.get_corrected_age_days(date_after_birthday),
+            -25  # 300 - 50 = 250, so corrected age is 5 - (280 - 250) = -25
+        )
+
+        date_after_due = baby_date.get_due_date() + timezone.timedelta(days=5)
+        self.assertEqual(
+            baby_date.get_corrected_age_days(date_after_due),
+            5
+        )
